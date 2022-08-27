826617c6c3245d470cf630b35638324b9633ef33
Suhail <gumchaa17@gmail.com>
2022-08-28 03:02:52 +0530

    update

--- Python/Python.py
+++ Python/Python.py
@@ -428,3 +428,49 @@ while i < 6:
     if i == 3:
         break
     i += 1
+
+    # Continue statement
+i = 0
+while i < 10:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+    
+# For Loops
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
+    
+    # Looping through range
+for x in range(2, 10, 2):
+    print(x)
+    
+for x in range(3, 30, 3):
+    print(x)
+else:
+    print("finally Finished")
+    
+for x in range(6):
+    if x == 3: break
+    print(x)
+else: 
+    print("Finally Finished!")
+    
+    # Nested Loops
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+    for y in fruits:
+        print(x, y)
